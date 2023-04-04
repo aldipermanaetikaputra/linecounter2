7166d784be84402774127fa65e9f43b77203736e
aldipermanaetikaputra <aldipermanaetikaputra@gmail.com>
2023-04-05 06:54:44 +0700

    Allow library to be executable

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,19 @@ import LineCounter, { LineCounterOptions } from './LineCounter.js';
 
 export { LineCounter, LineCounterOptions };
 
-const args = process.argv.slice(2);
-if (args.length) {
-  const argv = minimist(args) as Record<string, string>;
-  argv.filePath = argv.filePath || argv['_']?.[0];
-  if (argv.filePath) {
-    LineCounter.count(argv as unknown as LineCounterOptions)
-      .then(console.info)
-      .catch(console.error);
+const main = () => {
+  const args = process.argv.slice(2);
+  if (args.length) {
+    const argv = minimist(args) as Record<string, string>;
+    argv.filePath = argv.filePath || argv['_']?.[0];
+    if (argv.filePath) {
+      LineCounter.count(argv as unknown as LineCounterOptions)
+        .then(console.info)
+        .catch(console.error);
+    }
   }
+};
+
+if (require.main === module) {
+  main();
 }
